0edc8fec0e8bbe1feae0c64551ce3286eb4c987e
busido90 <nagatai0120@gmail.com>
2015-05-03 17:34:31 +0800

    Initial Commit

---
+++ 002_BijinJSQMessageView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  002_BijinJSQMessageView
+//
+//  Created by 永山 大志 on 2015/05/03.
+//  Copyright (c) 2015年 永山 大志. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
